b2ba446a3c741085a24fcec6ecb851d11e797d2f
Maurice Courtois <mcourtois@centos8.cig.baar>
2019-10-24 01:48:39 +0200

    - Add auto git clone of vtiger and a nice enjoy message

--- configure.sh
+++ configure.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-sudo chmod ugo+rw -fR vtigercrm/
-sudo chown 923 -fR vtigercrm/
+git clone https://code.vtiger.com/vtiger/vtigercrm.git
+#sudo chmod ugo+rw -fR vtigercrm/
+#sudo chown 923 -fR vtigercrm/
+. ./.env
+docker-compose up -d
+echo ""
+echo "Vtiger is now running on https://$VIRTUAL_HOST"
+echo ""
+echo "Enjoy!"
+echo ""
